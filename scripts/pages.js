5e104b42278896c9ad6b04580370403d4b1be47c
TacTalaPaca <136171243+TacTalaPaca@users.noreply.github.com>
2024-12-07 20:58:02 +0200

    Adaptive sidebar

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,21 +1,17 @@
-// Function to show a page
 function showPage(pageId) {
-    var pages = document.getElementsByClassName("content")[0].children;
-    for (var i = 0; i < pages.length; i++) {
+    const content = document.getElementsByClassName("content")[0];
+    const pages = content.children;
+    for (let i = 0; i < pages.length; i++) {
         pages[i].style.display = "none";
     }
     document.getElementById(pageId).style.display = "block";
 
-    // Store the selected page in localStorage
     localStorage.setItem('selectedPage', pageId);
 }
 // Check for user preference and apply it
 window.onload = function() {
-    const userSelectedPage = localStorage.getItem('selectedPage');
-    if (userSelectedPage) {
-        showPage(userSelectedPage);
-    } else {
-        // If there's no selected page in localStorage, default to 'pg1'
-        showPage('pg1');
-    }
+    const selectedPage = localStorage.getItem('selectedPage');
+    const defaultPage = 'pg1';
+    
+    showPage(selectedPage || defaultPage);
 };
